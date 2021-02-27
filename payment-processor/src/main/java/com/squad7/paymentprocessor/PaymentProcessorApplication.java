91b77ddea442e08aeecbcf50a4278a947ff53bd8
rkhemka <rajatkhemka66@gmail,com>
2021-02-27 10:27:09 -0600

    adding maven project

---
+++ payment-processor/src/main/java/com/squad7/paymentprocessor/PaymentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.squad7.paymentprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessorApplication.class, args);
+	}
+
+}
---
+++ payment-processor/src/test/java/com/squad7/paymentprocessor/PaymentProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad7.paymentprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
